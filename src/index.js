62009b93c0045267858b536911d74e787495c427
StefanTodoran <stefan.alex4@gmail.com>
2023-04-14 11:22:03 -0700

    initial commit, basic project structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+(function () {
+    window.addEventListener('load', init);
+    /**
+     * Initialization function that should handle anything that needs to occur on page load.
+     */
+    function init() {
+        console.log("test");
+    }
+})();
