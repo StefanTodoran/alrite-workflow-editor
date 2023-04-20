d751b307dedcf8dba1463a7810b0ecac1b74d8b1
StefanTodoran <stefan.alex4@gmail.com>
2023-04-20 00:00:25 -0700

    finished ts configuration, working on page components

--- src/components.ts
+++ src/components.ts
@@ -1,80 +1,82 @@
-// =============== \\
-// PAGE COMPONENTS \\
-// =============== \\
-
-interface PageComponent {
-  // Represents any page component that has a 
-  // visual element to display on the page.
-}
-
-interface MultipleChoice extends PageComponent {
-  text: string,
-  choices: Choice[],
-  multiselect: boolean,
-}
-
-interface Choice {
-  text: string,
-  link: string, // The pageID this option links to if selected (not valid if parent has multiselect enabled)
-  valueID: string, // Unique identifier e.g. "option_A", will store boolean representing if selected
-}
-
-interface TextInput extends PageComponent {
-  text: string,
-  type: string, // Type of input to accept, e.g. "numeric", "default"
-  units?: string, // If provided, displayed next to the TextInput, e.g. "cm"
-  valueID: string, // Must be unique, used for identifying the value inputted
-  defaultValue?: any, // Type depends on type property
-}
-
-interface Button extends PageComponent {
-  text?: string, // Defaults to "skip"
-  hint?: string, // Displayed above button detailing when/why to skip
-  link: string, // The pageID this button skips to if pressed
-}
-
-interface Counter extends PageComponent {
-  title: string,
-  hint?: string,
-  timeLimit: number, // Given in seconds
-  offerManualInput: boolean,
-}
-
-interface Modal extends PageComponent {
-  pageID: string, // Must be unique
-  // To link to a modal via it's pageID, a component must specify that ID and the modal must be on the same page
-  title: string,
-  content: string[], // Each string is displayed as a separate paragraph
-  buttons: Button[], // Navigation used to exit the modal
-  showCloseButton: boolean, // Whether to display a button that closes the modal, returning the the current (parent) page
-}
-
-interface Page {
-  pageID: string, // Must be unique
-  title: string,
-  content: PageComponent[],
-}
-
-// ============== \\
-// LOGIC HANDLING \\
-// ============== \\
-
-interface LogicComponent {
-  // Only one of these should exist per page, used to do conditional
-  // logic based on some value input on the page.
-}
-
-interface ComparisonLogic extends LogicComponent {
-  type: string, // Valid types include ">", "<", ">=", "<=", "="
-  valueID: string,
-  threshold: number,
-  satisfiedLink: string, // The pageIDs to link to given whether value satisfies the threshold given the comparison type
-  notSatisfiedLink: string,
-}
-
-interface SelectionLogic extends LogicComponent {
-  type: string, // Valid types include "all_selected", "at_least_one", "exactly_one", "none_selected"
-  valueIDs: string[],
-  satisfiedLink: string, // The pageIDs to link to given whether value satisfies the boolean condition
-  notSatisfiedLink: string,
+export module Components {
+  // =============== \\
+  // PAGE COMPONENTS \\
+  // =============== \\
+  
+  export interface PageComponent {
+    // Represents any page component that has a 
+    // visual element to display on the page.
+  }
+  
+  export interface MultipleChoice extends PageComponent {
+    text: string,
+    choices: Choice[],
+    multiselect: boolean,
+  }
+  
+  export interface Choice {
+    text: string,
+    link: string, // The pageID this option links to if selected (not valid if parent has multiselect enabled)
+    valueID: string, // Unique identifier e.g. "option_A", will store boolean representing if selected
+  }
+  
+  export interface TextInput extends PageComponent {
+    text: string,
+    type: string, // Type of input to accept, e.g. "numeric", "default"
+    units?: string, // If provided, displayed next to the TextInput, e.g. "cm"
+    valueID: string, // Must be unique, used for identifying the value inputted
+    defaultValue?: any, // Type depends on type property
+  }
+  
+  export interface Button extends PageComponent {
+    text?: string, // Defaults to "skip"
+    hint?: string, // Displayed above button detailing when/why to skip
+    link: string, // The pageID this button skips to if pressed
+  }
+  
+  export interface Counter extends PageComponent {
+    title: string,
+    hint?: string,
+    timeLimit: number, // Given in seconds
+    offerManualInput: boolean,
+  }
+  
+  export interface Modal extends PageComponent {
+    pageID: string, // Must be unique
+    // To link to a modal via it's pageID, a component must specify that ID and the modal must be on the same page
+    title: string,
+    content: string[], // Each string is displayed as a separate paragraph
+    buttons: Button[], // Navigation used to exit the modal
+    showCloseButton: boolean, // Whether to display a button that closes the modal, returning the the current (parent) page
+  }
+  
+  export interface Page {
+    pageID: string, // Must be unique
+    title: string,
+    content: PageComponent[],
+  }
+  
+  // ============== \\
+  // LOGIC HANDLING \\
+  // ============== \\
+  
+  export interface LogicComponent {
+    // Only one of these should exist per page, used to do conditional
+    // logic based on some value input on the page.
+  }
+  
+  export interface ComparisonLogic extends LogicComponent {
+    type: string, // Valid types include ">", "<", ">=", "<=", "="
+    valueID: string,
+    threshold: number,
+    satisfiedLink: string, // The pageIDs to link to given whether value satisfies the threshold given the comparison type
+    notSatisfiedLink: string,
+  }
+  
+  export interface SelectionLogic extends LogicComponent {
+    type: string, // Valid types include "all_selected", "at_least_one", "exactly_one", "none_selected"
+    valueIDs: string[],
+    satisfiedLink: string, // The pageIDs to link to given whether value satisfies the boolean condition
+    notSatisfiedLink: string,
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,77 @@
-'use strict';
-(function () {
+import { Components } from "./components";
 
-  window.addEventListener('load', init);
+window.addEventListener("load", init);
+var selectedPage = null;
 
-  /**
-   * Initialization function that should handle anything that needs to occur on page load.
-   */
-  function init() {
-    const dummyData = [
-      {}
-    ];
+/**
+ * Initialization function that should handle anything that needs to occur on page load.
+ */
+function init() {
+  console.log("INIT");
+
+  const dummyData = [
+    <Components.Page>{
+      pageID: "page_1",
+      title: "Page 1",
+      content: [],
+    },
+    <Components.Page>{
+      pageID: "page_2",
+      title: "Page 2",
+      content: [],
+    },
+    <Components.Page>{
+      pageID: "page_3",
+      title: "Page 3",
+      content: [],
+    },
+    <Components.Page>{
+      pageID: "page_4",
+      title: "Page 4",
+      content: [],
+    },
+    <Components.Page>{
+      pageID: "page_5",
+      title: "Page 5",
+      content: [],
+    },
+    <Components.Page>{
+      pageID: "page_6",
+      title: "Page 6",
+      content: [],
+    },
+    <Components.Page>{
+      pageID: "page_7",
+      title: "Page 7",
+      content: [],
+    },
+  ];
+
+  for (let i = 0; i < dummyData.length; i++) {
+    const page: Components.Page = dummyData[i];
+    addPageCard(page.pageID, page.title);
   }
-  
-})();
+}
+
+function addPageCard(id: string, title: string) {
+  const lastPosition: HTMLElement = document.getElementById("last-position");
+  const template: HTMLElement = document.getElementById("template-card");
+  const copy: Node = template.cloneNode(true);
+  document.querySelector("body").insertBefore(copy, lastPosition);
+
+  // Only this new card and the template will be hidden.
+  const card: Element = document.querySelectorAll(".page-card.hidden")[1];
+  card.querySelector("h1").textContent = title;
+  card.classList.remove("hidden");
+  card.id = id;
+}
+
+// Give a reference to a DOM element (specifically a page card),
+// creates a Page component for exporting purposes.
+function extractPageCard(card: HTMLElement) {
+  return <Components.Page>{
+    pageID: card.id,
+    title: card.querySelector("h1").textContent,
+    content: [] // TODO
+  };
+}
