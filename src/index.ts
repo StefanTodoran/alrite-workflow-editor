dfb2de64504ea1e3464898a2540595a0db254aa2
StefanTodoran <stefan.alex4@gmail.com>
2023-04-21 16:18:29 -0700

    page card movement and adding functionality done

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import { Components } from "./components";
 
 window.addEventListener("load", init);
-var selectedPage = null;
+var body: HTMLElement = null;
+var selectedCard: string = null;
 
 /**
  * Initialization function that should handle anything that needs to occur on page load.
  */
 function init() {
+  body = document.querySelector("body");
+
   const dummyData = [
     <Components.Page>{
       pageID: "page_1",
-      title: "Page 1",
+      title: "abc",
       content: [],
     },
     <Components.Page>{
       pageID: "page_2",
-      title: "Page 2",
+      title: "long long name",
       content: [],
     },
     <Components.Page>{
@@ -49,13 +52,35 @@ function init() {
     const page: Components.Page = dummyData[i];
     addPageCard(page.pageID, page.title);
   }
+  updatePageCardMoveButtons();
+
+  document.getElementById("add-page-button").addEventListener("click", addNewPage);
 }
 
+let newPageIndex = 0;
+function addNewPage() {
+  const page = <Components.Page>{
+    pageID: `new_page_${newPageIndex}`,
+    title: "Blank Page",
+    content: [],
+  };
+  addPageCard(page.pageID, page.title);
+  updatePageCardMoveButtons();
+
+  newPageIndex++;
+  window.scrollTo({
+    left: body.scrollWidth,
+    behavior: "smooth",
+  });
+}
+
+// Creates and adds a page card to the DOM, before the add button.
+// Populates it with the id and title and adds event listeners.
 function addPageCard(id: string, title: string) {
-  const lastPosition: HTMLElement = document.getElementById("last-position");
-  const template: HTMLElement = document.getElementById("template-card");
-  const copy: Node = template.cloneNode(true);
-  document.querySelector("body").insertBefore(copy, lastPosition);
+  const addButton = document.getElementById("add-page-button");
+  const template = document.getElementById("template-card");
+  const copy = template.cloneNode(true);
+  body.insertBefore(copy, addButton);
 
   // Only this new card and the template will be hidden.
   const card: Element = document.querySelectorAll(".page-card.hidden")[1];
@@ -63,6 +88,47 @@ function addPageCard(id: string, title: string) {
   card.querySelector("h2").textContent = id;
   card.classList.remove("hidden");
   card.id = id;
+
+  const editCard = card.querySelector(".edit-button");
+  editCard.addEventListener("click", () => {
+    selectedCard = selectedCard === card.id ? null : card.id;
+    updateSelectedCard();
+  });
+
+  const moveLeft = card.querySelector(".move-left-button");
+  moveLeft.addEventListener("click", () => {
+    body.insertBefore(card, card.previousSibling);
+    updatePageCardMoveButtons();
+  });
+
+  const moveRight = card.querySelector(".move-right-button");
+  moveRight.addEventListener("click", () => {
+    body.insertBefore(card, card.nextSibling.nextSibling);
+    updatePageCardMoveButtons();
+  });
+}
+
+// Updates the event listeners for ever page card, ensure the first page card 
+// does not have move left enabled, nor the last card have more right enabled.
+function updatePageCardMoveButtons() {
+  const cards = document.querySelectorAll(".page-card:not(.hidden)");
+
+  cards.forEach((card, index) => {
+    const moveLeft = card.querySelector(".move-left-button");
+    const moveRight = card.querySelector(".move-right-button");
+
+    if (index === 0) {
+      moveLeft.classList.add("disabled");
+    } else {
+      moveLeft.classList.remove("disabled");
+    }
+
+    if (index === cards.length - 1) {
+      moveRight.classList.add("disabled");
+    } else {
+      moveRight.classList.remove("disabled");
+    }
+  });
 }
 
 // Give a reference to a DOM element (specifically a page card),
@@ -73,4 +139,14 @@ function extractPageCard(card: HTMLElement) {
     title: card.querySelector("h1").textContent,
     content: [] // TODO
   };
+}
+
+// When a card is selected to edit, run this function
+// to update the css styles accordingly.
+function updateSelectedCard() {
+  const newSelected = document.getElementById(selectedCard);
+  const allCards = document.querySelectorAll(".page-card");
+
+  allCards.forEach(card => card.classList.remove("selected"));
+  if (newSelected) newSelected.classList.add("selected");
 }
